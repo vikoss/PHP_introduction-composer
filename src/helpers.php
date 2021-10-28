d894282587763b0803e79e7f289c1c7156a43a62
vikoss <vikoss.yiss@gmail.com>
2021-10-27 19:10:56 -0500

    validate if the functions exist

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,15 @@
 <?php
 
-function upper($value)
-{
-    return Text\Format::upperText($value);
+if (!function_exists('upper')) {
+    function upper($value)
+    {
+        return Text\Format::upperText($value);
+    }
 }
 
-function lower($value)
-{
-    return Text\Format::lowerText($value);
+if (!function_exists('lower')) {
+    function lower($value)
+    {
+        return Text\Format::lowerText($value);
+    }
 }
